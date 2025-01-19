64fbaa72e111fe090f049f14a2f5d25d97cd7d30
Gabriel <gmonteiro810@gmal.com>
2025-01-19 14:48:32 -0300

    feat(todoApp): configure DatabaseContext and AutoMapper in Program.cs

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
