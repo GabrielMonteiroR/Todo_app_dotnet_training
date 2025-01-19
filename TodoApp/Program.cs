4439ce043f3c25bdff5cad03dc7515acb7a99747
Gabriel <gmonteiro810@gmal.com>
2025-01-19 14:49:42 -0300

    feat(todoApp): add Swagger support for API documentation in development

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
@@ -9,7 +11,20 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API TODO v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapControllers();
 
